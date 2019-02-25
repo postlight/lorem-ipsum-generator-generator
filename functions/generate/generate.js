144820221bbe3bd1ada13ab9d1cf4a313596790b
Drew Bell <drew.bell@postlight.com>
2019-02-25 15:30:37 -0600

    refactor: tidy up generate

--- functions/generate/generate.js
+++ functions/generate/generate.js
@@ -1,21 +1,21 @@
 var path = require("path")
-var fs = require("fs")
 var markov = require("markov")
 var m = markov()
 var db = require(path.resolve(process.cwd(), 'db.json'))
+m.setDb(db);
 
-exports.handler = function(event, context, callback) {
-  m.setDb(db);
+exports.handler = function(event, _, callback) {
   function graf (words) {
     var length = (words / 2) || 50
     var chain = m.fill(m.pick(), length)
     return chain
       .join(" ")
       .split(".")
-      .map(s => s.trim())
-      .map(s => `${s.charAt(0).toUpperCase()}${s.slice(1)}`)
-      .join(". ")
-      .concat(".")
+      .map(s => {
+        s = s.trim()
+        return `${s.charAt(0).toUpperCase()}${s.slice(1)}.`
+      })
+      .join(" ")
   }
 
   var grafs = []
